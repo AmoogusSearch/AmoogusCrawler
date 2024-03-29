04b79f549ef60636c1cacdd167d5d94fdec69601
Jan <jan20202020@users.noreply.github.com>
2024-03-29 20:06:56 +0100

    getURLFromHTML and Tests

--- crawl.js
+++ crawl.js
@@ -1,13 +1,39 @@
+const { JSDOM } = require("jsdom");
+const cheerio = require("cheerio");
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
-    
-    let hostPath = `${urlObj.hostname}${urlObj.pathname}`
+
+    let hostPath = `${urlObj.hostname}${urlObj.pathname}`;
     if (hostPath.length > 0 && hostPath.endsWith("/")) {
         hostPath = hostPath.slice(0, -1);
     }
 
     return hostPath;
 }
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const $ = cheerio.load(htmlBody);
+    const linkElements = $("a");
+    linkElements.each((index, element) => {
+        const href = $(element).attr("href");
+        if (!href) return;
+        let urlObj;
+        try {
+            urlObj = new URL(href, baseURL); // convert to absolute URL
+            if (href.startsWith("/"))
+                new URL(`${baseURL}${href}`); // check if it's a valid URL
+            else new URL(href); // check if it's a valid URL
+        } catch (e) {
+            return console.error(`Error parsing URL: ${e.message}`);
+        }
+        urls.push(urlObj.href);
+    });
+    return urls;
+}
+
 module.exports = {
     normalizeURL,
+    getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsFromHTML } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL >  Strip Protocol", () => {
@@ -17,4 +17,80 @@ test("normalizeURL >  Capitals", () => {
     expect(normalizeURL("https://Example.com")).toBe("example.com");
     expect(normalizeURL("https://exAMple.cOm")).toBe("example.com");
     expect(normalizeURL("https://exAMple.cOm/TeSt")).toBe("example.com/TeSt"); // don't lowercase path
+});
+
+test("getURLsFromHTML >  Absolute", () => {
+    expect(
+        getURLsFromHTML(
+            `<html>
+        <body>
+            <a href="https://blog.example.com/path/">Link</a>
+        </body>
+    </html>`,
+            "https://blog.example.com"
+        )
+    ).toEqual(["https://blog.example.com/path/"]);
+});
+
+test("getURLsFromHTML >  Relative", () => {
+    expect(
+        getURLsFromHTML(
+            `<html>
+        <body>
+            <a href="/path/">Link</a>
+        </body>
+    </html>`,
+            "https://blog.example.com"
+        )
+    ).toEqual(["https://blog.example.com/path/"]);
+});
+
+
+test("getURLsFromHTML >  Relative & Absolute", () => {
+    expect(
+        getURLsFromHTML(
+            `<html>
+        <body>
+            <a href="/path1/">Link</a>
+            <a href="https://blog.example.com/path2/">Link</a>
+        </body>
+    </html>`,
+            "https://blog.example.com"
+        )
+    ).toEqual(["https://blog.example.com/path1/", "https://blog.example.com/path2/"]);
+});
+
+test("getURLsFromHTML >  Nested", () => {
+    expect(
+        getURLsFromHTML(
+            `<html>
+        <body>
+            
+            <div>
+                <a href="https://blog.example.com/path1/">Link</a>
+                <div>
+                    <p>
+                        <a href="/path2/">Link</a>
+                    </p>
+                </div>
+            </div>
+        </body>
+    </html>`,
+            "https://blog.example.com"
+        )
+    ).toEqual(["https://blog.example.com/path1/", "https://blog.example.com/path2/"]);
+});
+
+test("getURLsFromHTML >  Invalid", () => {
+    expect(
+        getURLsFromHTML(
+            `<html>
+        <body>
+            <a href="invalid">Link</a>
+            <a href="::#//invalid/x">Link</a>
+        </body>
+    </html>`,
+            "https://blog.example.com"
+        )
+    ).toEqual([]);
 });
