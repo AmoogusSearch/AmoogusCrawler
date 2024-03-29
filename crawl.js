197611f4d8d967f823eac06c54392119e6be5997
Jan <jan20202020@users.noreply.github.com>
2024-03-29 20:30:45 +0100

    Crawl Single Page & Error Handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,7 @@
 const { JSDOM } = require("jsdom");
 const cheerio = require("cheerio");
+const axios = require("axios");
+
 
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
@@ -33,7 +35,24 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return urls;
 }
 
+async function crawlPage(currentURL) {
+  
+  console.log(`\x1b[32mCrawling ${currentURL}`);
+
+  let response
+  try {
+    response = await axios.get(currentURL);
+  } catch (e) {
+    return console.error(`\x1b[31mError fetching URL: ${e.message}\x1b[0m`);
+  }
+  const contentType = response.headers["content-type"];
+  if (!contentType || !contentType.includes("text/html")) {
+    return console.error(`\x1b[33mSkipping non-HTML page: ${contentType}\x1b[0m`);
+  }
+}
+
 module.exports = {
     normalizeURL,
     getURLsFromHTML,
+    crawlPage,
 };
---
+++ main.js
@@ -0,0 +1,10 @@
+const { crawlPage } = require("./crawl")
+
+function main() {
+    
+    crawlPage("https://lornsenschule.de/sitemap.xml")
+}
+
+
+
+main()
